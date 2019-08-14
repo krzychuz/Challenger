649eb1187b13bd48abbda20a04ca66f37fc44cdb
Mateusz Tracz <Mateusz.Tracz@consafelogistics.com>
2019-08-14 10:29:48 +0200

    Add IIS Kestrel integration

--- Challenger/Challenger.Web/Program.cs
+++ Challenger/Challenger.Web/Program.cs
@@ -19,6 +19,8 @@ namespace Challenger.Web
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
       WebHost.CreateDefaultBuilder(args)
+        .UseIISIntegration()
+        .UseKestrel()
         .UseStartup<Startup>();
   }
 }
