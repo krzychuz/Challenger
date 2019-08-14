31c7d4a9fc46d5c89b7a52d5f3797e065a9b3dcd
Krzysiek Zabawa <krzysztof.zabawa@consafelogistics.com>
2019-08-14 09:33:38 +0200

    NavbBarBrand fix - Correct link to Consafe website

--- Challenger/Challenger.Web/ClientApp/src/components/NavMenu.js
+++ Challenger/Challenger.Web/ClientApp/src/components/NavMenu.js
@@ -26,7 +26,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="https://www.consafelogistics.com">Consafe Logistics</NavbarBrand>
+            <NavbarBrand href="https://www.consafelogistics.com">Consafe Logistics</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
