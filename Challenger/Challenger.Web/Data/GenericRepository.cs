6f43cb185fc2a8ee497026323ee4ff73197323bc
Mateusz Tracz <Mateusz.Tracz@consafelogistics.com>
2019-09-10 19:25:09 +0200

    Add Generic repository and Unit of work

---
+++ Challenger/Challenger.Web/Data/GenericRepository.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using LiteDB;
+using Microsoft.EntityFrameworkCore;
+
+namespace Challenger.Web.Data
+{
+   public class GenericRepository<T> : IGenericRepository<T> where T : class, new()
+   {
+        private readonly LiteDatabase context;
+        private readonly LiteCollection<T> dbSet;
+
+        public GenericRepository(LiteDatabase context)
+        {
+            this.context = context;
+            dbSet = context.GetCollection<T>(nameof(T));
+        }
+
+        public virtual T GetById(object id)
+        {
+            return dbSet.FindById(new BsonValue(id));
+        }
+
+        public virtual IEnumerable<T> Get(
+           Expression<Func<T, bool>> filter)
+        {
+            return dbSet.Find(filter);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.FindAll();
+        }
+
+        public void Edit(T entity)
+        {
+            dbSet.Update(entity);
+        }
+
+        public void Insert(T entity)
+        {
+            dbSet.Insert(entity);
+        }
+
+        public void Delete(object id)
+        {
+            dbSet.Delete(new BsonValue(id));
+        }
+
+        public void Save()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ Challenger/Challenger.Web/Data/IGenericRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Challenger.Web.Data
+{
+    public interface IGenericRepository<T>
+    {
+        T GetById(object id);
+
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null);
+
+        IEnumerable<T> GetAll();
+
+        void Edit(T entity);
+
+        void Insert(T entity);
+
+        void Delete(object id);
+
+        void Save();
+    }
+}
