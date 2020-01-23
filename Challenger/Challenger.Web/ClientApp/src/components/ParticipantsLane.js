b680899f096f610f85f7359a1a0ef96c217ec370
Krzysiek <krzychuz@vp.pl>
2020-01-23 22:51:15 +0100

    Possitiblity to add/remove teams

--- Challenger/Challenger.Web/ClientApp/src/components/ParticipantsLane.js
+++ Challenger/Challenger.Web/ClientApp/src/components/ParticipantsLane.js
@@ -7,12 +7,30 @@ const getListStyle = isDraggingOver => ({
     background: isDraggingOver ? '#007bff' : 'lightgrey',
 });
 
-
 export class ParticipantsLane extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleRemoveTeam = this.handleRemoveTeam.bind(this);
+    }
+
+    handleRemoveTeam() {
+        fetch("api/Teams/teams/" + this.props.teamId, {
+            method: "DELETE",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+              }
+        })
+            .then(() => {
+                // TODO: callback to refresh teams grid instead of reloading the page
+                window.location.reload();
+            });
+    }
 
     render() {
         return (
-            <Droppable droppableId={this.props.teamId}>
+            <Droppable droppableId={String(this.props.teamId)}>
             {(provided, snapshot) => (
                 <div
                     ref={provided.innerRef}
@@ -26,6 +44,7 @@ export class ParticipantsLane extends Component {
                         />
                     ))}
                     {provided.placeholder}
+                    <button type="submit" className="btn btn-danger bottom-spacing-medium" onClick={this.handleRemoveTeam}>Delete</button>
                 </div>
             )}
         </Droppable>
