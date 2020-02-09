cda561dfffaedb50e819bcb0a35b7f3af0c04d5f
Krzysiek <krzychuz@vp.pl>
2020-02-09 07:49:56 +0100

    Snapshot creator added

---
+++ Challenger/Challenger.Web/Models/IndividualScoreSnapshot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenger.Web.Models
+{
+    public class IndividualScoreSnapshot
+    {
+        public IList<Participant> Participants { get; set; }
+
+        public DateTime SnapshotDate { get; set; }
+    }
+}
---
+++ Challenger/Challenger.Web/Models/TeamScoreSnapshot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenger.Web.Models
+{
+    public class TeamScoreSnapshot
+    {
+        public IList<Team> Teams { get; set; }
+
+        public DateTime SnapshotDate { get; set; }
+    }
+}
